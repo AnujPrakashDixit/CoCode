1b6b2e2481028f993ac65fef247305ff623798ae
Anuj Prakash Dixit <anujprakashdixit@gmail.com>
2025-12-06 17:04:58 +0530

    axios

--- backend/index.js
+++ backend/index.js
@@ -8,21 +8,21 @@ import axios from 'axios';
 const app = express();
 const server = http.createServer(app);
 
-// const url = `https://co-code-real-time-collaborative-ide.onrender.com`;
-// const interval = 30000;
+const url = `https://co-code-real-time-collaborative-ide.onrender.com`;
+const interval = 30000;
 
-// function reloadWebsite() {
-//   axios
-//     .get(url)
-//     .then((response) => {
-//       console.log("website reloded");
-//     })
-//     .catch((error) => {
-//       console.error(`Error : ${error.message}`);
-//     });
-// }
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
 
-// setInterval(reloadWebsite, interval);
+setInterval(reloadWebsite, interval);
 
 const io = new Server(server, {
   cors: { origin: "*" },
